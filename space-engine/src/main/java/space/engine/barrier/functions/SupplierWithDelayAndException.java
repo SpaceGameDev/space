2f66847cdd35e66b04774e0acd1d16f78c0c02b7
Firestar99 <31222740+firestar99@users.noreply.github.com>
2019-12-25 14:22:17 +0100

    [functions] cleaned up functions

--- space-engine/src/main/java/space/engine/barrier/functions/SupplierWithDelayAndException.java
+++ space-engine/src/main/java/space/engine/barrier/functions/SupplierWithDelayAndException.java
@@ -2,10 +2,8 @@ package space.engine.barrier.functions;
 
 import space.engine.barrier.DelayTask;
 
-//thenFutureWithException
 @FunctionalInterface
-public
-interface SupplierWithDelayAndException<T, EX extends Throwable> {
+public interface SupplierWithDelayAndException<T, EX extends Throwable> {
 	
 	T get() throws DelayTask, EX;
 }
