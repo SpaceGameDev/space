ad4508d19604009eaf58cc52bab555da8fd33d2c
Firestar99 <31222740+firestar99@users.noreply.github.com>
2020-01-01 17:34:00 +0100

    [vector] added Vector2 and Vector2i

---
+++ space-engine/src/main/java/space/engine/vector/conversion/ToVector2.java
@@ -0,0 +1,8 @@
+package space.engine.vector.conversion;
+
+import space.engine.vector.Vector2;
+
+public interface ToVector2 {
+	
+	Vector2 toVector2();
+}
---
+++ space-engine/src/main/java/space/engine/vector/conversion/ToVector2i.java
@@ -0,0 +1,8 @@
+package space.engine.vector.conversion;
+
+import space.engine.vector.Vector2i;
+
+public interface ToVector2i {
+	
+	Vector2i toVector2i();
+}
